37576c6416802590742c909d0a5b0c78ce00794b
Leandro <42415507+Leandrovar25@users.noreply.github.com>
2018-08-24 12:49:25 -0300

    Test multiplicacion

--- Proyect 1/src/Calculadora/TestCalculadora.java
+++ Proyect 1/src/Calculadora/TestCalculadora.java
@@ -17,4 +17,11 @@ public void testresta() {
 	int resultado = Calculadora.resta(7, 7);
 	assertEquals(0,resultado);
 	}
+
+@Test
+public void testmultiplcacion() {
+	int resultado = Calculadora.multiplicar(2,7);
+	assertEquals(14,resultado);
+	}
+
 }
